a92e0f4ede73e919dff4818ca702cb33614dfcec
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 01:26:33 +0800

    Add instruction and pseudocode

---
+++ new_list_from_two_lists_using_condition.py
@@ -0,0 +1,19 @@
+# Create a new list from two list using the following condition
+
+# Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
+# pseudocode
+
+# write the two given lists
+
+# create a new list
+
+# for each number in the first list
+#     if the number is odd
+#         add the number to the new list
+
+# for each number in the second list
+#     if the number is odd
+#         add the number to the new list
+
+# print the new list
